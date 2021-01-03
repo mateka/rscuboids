b9de51e6c818f3008fda1637eb304ab3cca9fbdf
Mateusz Adamczyk <matek_a@outlook.com>
2021-01-03 16:57:38 +0100

    Added physics plugin

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,31 @@ use bevy::{
     diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
     prelude::*,
 };
+use bevy_rapier2d::physics::RapierPhysicsPlugin;
+use bevy_rapier2d::rapier::{dynamics::RigidBodyBuilder, geometry::ColliderBuilder};
 use rand::{rngs::StdRng, Rng, SeedableRng};
 
-/// This example spawns a large number of cubes, each with its own changing position and material
-/// This is intended to be a stress test of bevy's ability to render many objects with different properties
-/// For the best results, run it in release mode: ```cargo run --example spawner --release
-/// NOTE: Bevy still has a number of optimizations to do in this area. Expect the performance here to go way up in the future
+use bevy_rapier2d::render::RapierRenderPlugin;
+
+#[bevy_main]
 fn main() {
     App::build()
+        .add_resource(WindowDescriptor {
+            title: "rsCuboids".to_string(),
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin)
+        .add_plugin(RapierRenderPlugin)
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_startup_system(setup.system())
-        .add_system(move_cubes.system())
+        .add_startup_system(setup_world.system())
+        .add_startup_system(setup_cubes.system())
         .add_system(text_update_system.system())
         .run();
 }
 
-fn move_cubes(
-    time: Res<Time>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    mut query: Query<(&mut Transform, &Handle<StandardMaterial>)>,
-) {
-    for (mut transform, material_handle) in query.iter_mut() {
-        let material = materials.get_mut(material_handle).unwrap();
-        transform.translation += Vec3::new(1.0, 0.0, 0.0) * time.delta_seconds();
-        material.albedo =
-            Color::BLUE * Vec3::splat((3.0 * time.seconds_since_startup() as f32).sin());
-    }
-}
-
-fn setup(
-    commands: &mut Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    asset_server: Res<AssetServer>,
-) {
+fn setup(commands: &mut Commands, asset_server: Res<AssetServer>) {
     commands
         // light
         .spawn(LightBundle {
@@ -45,7 +35,7 @@ fn setup(
         })
         // camera
         .spawn(Camera3dBundle {
-            transform: Transform::from_translation(Vec3::new(0.0, 15.0, 150.0))
+            transform: Transform::from_translation(Vec3::new(0.0, -15.0, 150.0))
                 .looking_at(Vec3::default(), Vec3::unit_y()),
             ..Default::default()
         })
@@ -68,27 +58,58 @@ fn setup(
             ..Default::default()
         })
         .with(FpsText);
+}
 
+fn setup_world(
+    commands: &mut Commands,
+    mut physics_config: ResMut<bevy_rapier2d::physics::RapierConfiguration>,
+) {
+    physics_config.gravity = bevy_rapier2d::na::Vector2::new(0.0, 0.0);
+
+    let ceiling_body = RigidBodyBuilder::new_static().translation(0.0, 65.0);
+    let ceiling_collider = ColliderBuilder::cuboid(1000.0, 1.0);
+    commands.spawn((ceiling_body, ceiling_collider));
+
+    let floor_body = RigidBodyBuilder::new_static().translation(0.0, -61.0);
+    let floor_collider = ColliderBuilder::cuboid(1000.0, 1.0);
+    commands.spawn((floor_body, floor_collider));
+
+    let left_body = RigidBodyBuilder::new_static().translation(-105.0, 0.0);
+    let left_collider = ColliderBuilder::cuboid(1.0, 1000.0);
+    commands.spawn((left_body, left_collider));
+
+    let right_body = RigidBodyBuilder::new_static().translation(105.0, 0.0);
+    let right_collider = ColliderBuilder::cuboid(1.0, 1000.0);
+    commands.spawn((right_body, right_collider));
+}
+
+fn setup_cubes(
+    commands: &mut Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     let mut rng = StdRng::from_entropy();
-    let cube_handle = meshes.add(Mesh::from(shape::Cube { size: 1.0 }));
-    for _ in 0..10000 {
-        commands.spawn(PbrBundle {
-            mesh: cube_handle.clone(),
-            material: materials.add(StandardMaterial {
-                albedo: Color::rgb(
-                    rng.gen_range(0.0..1.0),
-                    rng.gen_range(0.0..1.0),
-                    rng.gen_range(0.0..1.0),
-                ),
+    let cube_handle = meshes.add(Mesh::from(shape::Cube { size: 2.0 }));
+    for _ in 0..50 {
+        let position = Vec3::new(rng.gen_range(-50.0..50.0), rng.gen_range(-50.0..50.0), 0.0);
+        let body = RigidBodyBuilder::new_dynamic()
+            .translation(position.x, position.y)
+            .linvel(rng.gen_range(-30.0..30.0), rng.gen_range(-30.0..30.0));
+        let collider = ColliderBuilder::cuboid(1.0, 1.0).restitution(1.5);
+        commands
+            .spawn(PbrBundle {
+                mesh: cube_handle.clone(),
+                material: materials.add(StandardMaterial {
+                    albedo: Color::rgb(
+                        rng.gen_range(0.0..1.0),
+                        rng.gen_range(0.0..1.0),
+                        rng.gen_range(0.0..1.0),
+                    ),
+                    ..Default::default()
+                }),
                 ..Default::default()
-            }),
-            transform: Transform::from_translation(Vec3::new(
-                rng.gen_range(-50.0..50.0),
-                rng.gen_range(-50.0..50.0),
-                0.0,
-            )),
-            ..Default::default()
-        });
+            })
+            .with_bundle((body, collider));
     }
 }
 
